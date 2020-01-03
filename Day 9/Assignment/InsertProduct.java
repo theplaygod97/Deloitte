5ff9569165211fb95aac60768b01f59b857ed06b
Satyabrat Sahoo <sasgt97@gmail.com>
2020-01-03 14:19:24 +0530

    Update InsertProduct.java

--- Day 9/Assignment/InsertProduct.java
+++ Day 9/Assignment/InsertProduct.java
@@ -7,18 +7,17 @@ public class InsertProduct {
 		Product product = new Product();
 		product.accept();
 		Connection connection = DBConnection.makeConnection();
-		//Statement statement = connection.createStatement();
-		//String insertQuery = "insert into hr.product values(1,'Soap',10,50)";
 		PreparedStatement statement =
 				connection.prepareStatement("insert into hr.product values(?,?,?,?)");
 				statement.setInt(1, product.getProductId());
 				statement.setString(2, product.getProductName());
 				statement.setInt(3, product.getPrice());
 				statement.setInt(4, product.getQOH());
-				
-				//statement.executeUpdate();	
+			
 		int rowsAffected = statement.executeUpdate();
 		System.out.println("Insert success with "+rowsAffected+" rows affected.");
+		statement.close();
+		connection.close();
 	}
 
 }
